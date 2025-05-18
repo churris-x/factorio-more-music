9886b590070810a71223bf6a592d64cf8f85116e
Francisco Altimari <francisco@altimari.nl>
2025-05-18 11:05:49 -0300

    Add some songs to ambient-sound

---
+++ data.lua
@@ -0,0 +1,50 @@
+-- log(serpent.block(data.raw["ambient-sound"]))
+
+-- remove default songs
+data.raw["ambient-sound"] = {}
+
+-- add songs
+data:extend{{
+    name = "sc-01-opening",
+    type = "ambient-sound",
+    track_type = "main-track",
+    planet = "nauvis",
+    weight = 1,
+    sound = "__more-music__/music/sc-01-opening.ogg",
+},{
+    name = "sc-02-under-construction",
+    type = "ambient-sound",
+    track_type = "interlude",
+    planet = "nauvis",
+    weight = 1,
+    sound = "__more-music__/music/sc-02-under-construction.ogg",
+},{
+    name = "sc-04-some-elements",
+    type = "ambient-sound",
+    track_type = "main-track",
+    planet = "nauvis",
+    weight = 1,
+    sound = "__more-music__/music/sc-04-some-elements.ogg",
+},{
+    name = "sc-05-discovery",
+    type = "ambient-sound",
+    track_type = "interlude",
+    planet = "nauvis",
+    weight = 1,
+    sound = "__more-music__/music/sc-05-discovery.ogg",
+},{
+    -- Menu track
+    name = "sc-07-new-world",
+    type = "ambient-sound",
+    track_type = "menu-track",
+    planet = "nauvis",
+    weight = 1,
+    sound = "__more-music__/music/sc-07-new-world.ogg",
+},{
+    name = "sc-08-some-calm",
+    type = "ambient-sound",
+    track_type = "interlude",
+    planet = "nauvis",
+    weight = 1,
+    sound = "__more-music__/music/sc-08-some-calm.ogg",
+}}
